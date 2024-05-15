267c5fc5a2868fb684cd9c9d380a211e08c7c252
Simon Cozens <simon@simon-cozens.org>
2024-05-15 14:39:25 +0100

    Center guides on coordinate

--- Lib/fontprimer/guidelines.py
+++ Lib/fontprimer/guidelines.py
@@ -4,14 +4,20 @@ from glyphsLib.classes import GSLINE, GSFont, GSNode, GSPath, GSGlyph, GSLayer
 # from https://github.com/mekkablue/Glyphs-Scripts/blob/a4421210dd17305e3205b7ca998cab579b778bf6/Paths/Fill%20Up%20with%20Rectangles.py
 def drawRect(myBottomLeft, myTopRight):
     myRect = GSPath()
-    myCoordinates = [[myBottomLeft[0], myBottomLeft[1]], [myTopRight[0], myBottomLeft[1]], [myTopRight[0], myTopRight[1]], [myBottomLeft[0], myTopRight[1]]]
+    myCoordinates = [
+        [myBottomLeft[0], myBottomLeft[1]],
+        [myTopRight[0], myBottomLeft[1]],
+        [myTopRight[0], myTopRight[1]],
+        [myBottomLeft[0], myTopRight[1]],
+    ]
 
     for thisPoint in myCoordinates:
-      newNode = GSNode((thisPoint[0], thisPoint[1]), GSLINE)
-      myRect.nodes.append(newNode)
+        newNode = GSNode((thisPoint[0], thisPoint[1]), GSLINE)
+        myRect.nodes.append(newNode)
 
     myRect.closed = True
     return myRect
+
 
 def decomposed_paths(layer):
     newpaths = [l.clone() for l in layer.paths]
@@ -23,10 +29,12 @@ def decomposed_paths(layer):
         newpaths += to_append
     return newpaths
 
+
 def decompose_all_layers(font):
     for glyph in font.glyphs:
         for layer in glyph.layers:
             layer.shapes = list(layer.paths) + decomposed_paths(layer)
+
 
 def add_guidelines(font, args):
     for glyph in font.glyphs:
@@ -37,7 +45,9 @@ def add_guidelines(font, args):
         # Skip combining marks
         for layer in glyph.layers:
             # If this has any components which don't start in "_part", skip it
-            if layer.components and not all(component.name.startswith("_part") for component in layer.components):
+            if layer.components and not all(
+                component.name.startswith("_part") for component in layer.components
+            ):
                 continue
             # Skip anything with underscore anchors
             if any(anchor.name.startswith("_") for anchor in layer.anchors):
@@ -45,16 +55,17 @@ def add_guidelines(font, args):
             master = layer.master
             heightsAndThicknesses = [
                 (master.descender, args.default_thickness),
-                (0, args.thicker_thickness),
+                (0 + args.thicker_thickness / 2, args.thicker_thickness),
                 (master.xHeight, args.default_thickness),
-                (master.ascender, args.default_thickness)
+                (master.ascender, args.default_thickness),
             ]
-            
+
             for height, thickness in heightsAndThicknesses:
-                bottomLeft = (-args.overlap, height)
-                topRight = (layer.width+args.overlap, height+thickness)
+                bottomLeft = (-args.overlap, height - thickness / 2)
+                topRight = (layer.width + args.overlap, height + thickness / 2)
                 layerRect = drawRect(bottomLeft, topRight)
                 layer.shapes.append(layerRect)
+
 
 def add_guideline_glyph(font, thickness):
     if "_guide" in font.glyphs:
@@ -65,25 +76,37 @@ def add_guideline_glyph(font, thickness):
         layer.associatedMasterId = master.id
         layer.layerId = master.id
         layer.width = 1000
-        layer.shapes = [ GSPath() ]
+        layer.shapes = [GSPath()]
         layer.shapes[0].nodes = [
-                GSNode((0, 0), GSLINE),
-                GSNode((1000, 0), GSLINE),
-                GSNode((1000, thickness), GSLINE),
-                GSNode((0, thickness), GSLINE)
-            ]
+            GSNode((0, 0), GSLINE),
+            GSNode((1000, 0), GSLINE),
+            GSNode((1000, thickness), GSLINE),
+            GSNode((0, thickness), GSLINE),
+        ]
         font.glyphs["_guide"].layers.append(layer)
     pass
 
+
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("font", help="Glyphs file", metavar="GLYPHS")
-    parser.add_argument("--color", action="store_true", help="Output a single stroke for color processing")
+    parser.add_argument(
+        "--color",
+        action="store_true",
+        help="Output a single stroke for color processing",
+    )
     parser.add_argument("--output", "-o", help="Output file", metavar="GLYPHS")
-    parser.add_argument("--overlap", help="Overlap of the guidelines", type=int, default=10)
-    parser.add_argument("--default-thickness", help="Default guideline thickness", type=int, default=16)
-    parser.add_argument("--thicker-thickness", help="Thicker guideline thickness", type=int, default=32)
+    parser.add_argument(
+        "--overlap", help="Overlap of the guidelines", type=int, default=10
+    )
+    parser.add_argument(
+        "--default-thickness", help="Default guideline thickness", type=int, default=16
+    )
+    parser.add_argument(
+        "--thicker-thickness", help="Thicker guideline thickness", type=int, default=32
+    )
     args = parser.parse_args()
 
     if not args.output:
@@ -94,5 +117,5 @@ if __name__ == "__main__":
     else:
         decompose_all_layers(font)
         add_guidelines(font, args)
-    print("Saving on "+args.output)
+    print("Saving on " + args.output)
     font.save(args.output)
